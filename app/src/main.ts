a1c5c8ce5aa35fe5d037cdef43ad2d26ab2a499f
plasticut <plasticut@gmail.com>
2019-09-10 16:21:44 +0300

    +Swagger

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,11 +1,22 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const logger = new Logger('nest-test-app');
 
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('NestJS test application')
+    .setDescription('NestJS test application description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listenAsync(3000);
 
   logger.log('App started at http://localhost:3000/');
